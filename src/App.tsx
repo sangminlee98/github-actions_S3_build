d9aaa6da8cc9e3825917791660c594fa4569a1f9
sangminlee98 <tkdals0975@naver.com>
2022-09-05 03:19:56 +0900

    styling mainpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import styled from "styled-components";
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <Container>
+      <InnerContainer>
+        <h1>GITHUB ACTIONS PRACTICE</h1>
+        <a href="https://github.com/sangminlee98/github-actions_S3_build">
+          깃허브 이동
+        </a>
+      </InnerContainer>
+    </Container>
+  );
 }
 
 export default App;
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10%;
+`;
+const InnerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  & > a {
+    color: #9bb6fb;
+    margin-top: 30px;
+  }
+`;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createGlobalStyle } from "styled-components";
+import App from "./App";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    background-color: #eeeeee;
+  }
+`;
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <App />
+    <GlobalStyle />
   </React.StrictMode>
 );
